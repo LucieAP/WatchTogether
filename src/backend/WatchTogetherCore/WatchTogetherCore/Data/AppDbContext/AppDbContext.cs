9a659203e81b46362f4edd2dc1131aa43ac0784f
LucieAP <toshaletvinov1@gmail.com>
2025-01-28 17:11:10 +0300

    Обновил DbContext и создал Миграции

--- src/backend/WatchTogetherCore/WatchTogetherCore/Data/AppDbContext/AppDbContext.cs
+++ src/backend/WatchTogetherCore/WatchTogetherCore/Data/AppDbContext/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using WatchTogetherCore.Models;
 
 namespace WatchTogetherCore.Data.AppDbContext
 {
@@ -6,5 +8,36 @@ namespace WatchTogetherCore.Data.AppDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<User> Users {  get; set; } 
+        public DbSet<Room> Rooms {  get; set; } 
+        public DbSet<Participant> Participants {  get; set; } 
+        public DbSet<Message> Messages {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка составного ключа для Participant, состоящий из двух полей: UserId и RoomId.
+            modelBuilder.Entity<Participant>()
+                .HasKey(p => new { p.UserId, p.RoomId });
+
+            // Настройка связей для Room → User
+            modelBuilder.Entity<Room>()
+                .HasOne(r => r.CreatedByUser)            // У комнаты есть один создатель (User)
+                .WithMany(u => u.CreatedRooms)           // У пользователя может быть мclearного созданных комнат
+                .HasForeignKey(r => r.CreatedByUserId)   // Внешний ключ: CreatedByUserId в таблице Rooms
+                .OnDelete(DeleteBehavior.Restrict);      // Запрет каскадного удаления
+
+            // Настройка индексов
+            modelBuilder.Entity<User>()     // Уникальный индекс для User.Username (логины не повторяются)
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<Room>()     // Неуникальный индекс для Room.RoomName(ускорение поиска по имени комнаты).
+                .HasIndex(r => r.RoomName);
+        }
+
+        
     }
 }
+
+
+
