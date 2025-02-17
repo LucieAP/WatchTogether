762921e689c20da1ea98304b49ec70ff1721afbb
LucieAP <toshaletvinov1@gmail.com>
2025-02-17 17:03:51 +0300

    Изменил размер чата

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -60,7 +60,7 @@ export default function App() {
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
-        <Footer />
+        {/*<Footer />*/}
       </div>
     </Router>
   );
