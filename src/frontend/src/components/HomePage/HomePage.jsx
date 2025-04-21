0cd0003cbc3f40298d7b1b5536ad7647b92c6324
LucieAP <toshaletvinov1@gmail.com>
2025-04-21 19:59:07 +0300

    Вынеc метод formatDate в utils/homePageHelpers.js

--- src/frontend/src/components/HomePage/HomePage.jsx
+++ src/frontend/src/components/HomePage/HomePage.jsx
@@ -4,18 +4,7 @@ import { getPublicRooms, getUserRooms, deleteRoom } from "../../api/rooms";
 import { handleManualLeave } from "../../api/leaveRoomAction";
 import { useAuth } from "../../context/AuthContext";
 import "./HomePage.css"; // Импортируем файл стилей
-
-// Форматирование даты (вынесено за компонент)
-const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  return date.toLocaleString("ru-RU", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-};
+import { formatDate } from "../RoomPage/utils/homePageHelpers";
 
 const HomePage = () => {
   const navigate = useNavigate();
