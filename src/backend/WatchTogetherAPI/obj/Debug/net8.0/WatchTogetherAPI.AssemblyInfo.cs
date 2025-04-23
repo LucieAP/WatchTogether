0db986883a4d81caa67ec03450205f0ee9c31e71
LucieAP <toshaletvinov1@gmail.com>
2025-04-23 22:56:20 +0300

    Удалены конфиденциальные файлы из репозитория

--- src/backend/WatchTogetherAPI/obj/Debug/net8.0/WatchTogetherAPI.AssemblyInfo.cs
+++ src/backend/WatchTogetherAPI/obj/Debug/net8.0/WatchTogetherAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchTogetherAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0951c56b83eeb05156894dc45793482db3441e9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b436fbaa90ee1a7036bddf56268d2ef3dc97e62d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchTogetherAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchTogetherAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
